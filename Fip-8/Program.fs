882f7f6ab182a7dc2ae293e91ac12d1d29215bcb
nickkossolapov <nickkossolapov@gmail.com>
2025-09-16 20:25:28 +0100

    Add RayLib to draw to screen

--- Fip-8/Program.fs
+++ Fip-8/Program.fs
@@ -1,2 +1,14 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open Raylib_cs
+
+Raylib.InitWindow (800, 480, "Fip-8")
+
+let runProgramLoop () : bool =
+    not (CBool.op_Implicit (Raylib.WindowShouldClose ()))
+
+while runProgramLoop () do
+    Raylib.BeginDrawing ()
+    Raylib.ClearBackground Color.White
+    Raylib.DrawText ("Hello world", 12, 12, 20, Color.Black)
+    Raylib.EndDrawing ()
+
+Raylib.CloseWindow ()
